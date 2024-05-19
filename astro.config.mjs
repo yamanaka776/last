b3cabb200394bad1f0ee7e44a71168bcad8a4dca
yamanaka776 <92915718+yamanaka776@users.noreply.github.com>
2024-05-19 01:59:56 +0000

    font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+	integrations: [tailwind(), robotsTxt({sitemap:'https://ear-diary.comsitemap-index.xml'}), sitemap()],
+  site: 'https://ear-diary.com'
 });
