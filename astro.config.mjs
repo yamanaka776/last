7c6e0cd40793131e1620c33c9e08d93eeccee9f5
yamanaka776 <92915718+yamanaka776@users.noreply.github.com>
2024-05-20 14:05:13 +0000

    記事一覧作った

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,24 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
+import playformCompress from "@playform/compress";
+import playformInline from "@playform/inline";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(), robotsTxt({sitemap:'https://ear-diary.comsitemap-index.xml'}), sitemap()],
+  integrations: [tailwind(), robotsTxt({
+    sitemap: 'https://ear-diary.comsitemap-index.xml'
+  }), sitemap(), playformCompress(), playformInline(), partytown(
+	  { config: {
+        forward: ["dataLayer.push"],
+	  } ,},),],
   site: 'https://ear-diary.com',
-	image:{ 
-	remotePatters: [{
-		  hostname:'**.microcms-assets.io'}]}
+  image: {
+    remotePatters: [{
+      hostname: '**.microcms-assets.io'
+    }]
+  }
 });
