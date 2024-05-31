30a9582573f9524fd079b5af9977591616e75861
satoru <contac@ear-diary.com>
2024-05-31 14:00:33 +0000

    style

--- src/pages/og/[slug].png.ts
+++ src/pages/og/[slug].png.ts
@@ -1,17 +1,29 @@
-import type { APIContext } from "astro";
-import { getCollection, getEntryBySlug } from "astro:content";
-import { getOgImage } from "../../components/OgImage";
+import type { APIContext } from 'astro';
+import { getCollection } from 'astro:content';
+import { getOgImage } from '../../components/OgImage';
 
 export async function getStaticPaths() {
-  const posts = await getCollection("blog");
+  const posts = await getCollection('blog');
+
   return posts.map((post) => ({
-    params: { slug: post.slug },
+    params: {
+      slug: post.slug,
+    },
   }));
 }
 
-export async function get({ params }: APIContext) {
-  const post = await getEntryBySlug("blog", params.slug);
-  const body = await getOgImage(post?.data.title ?? "No title");
+export async function GET({ params }: APIContext) {
+  const { slug } = params;
+  if (!slug) return { status: 404 };
 
-  return { body, encoding: "binary" };
+  const post = (await getCollection('blog')).find((post) => post.slug === slug);
+  if (!post) return { status: 404 };
+
+  const body = await getOgImage(post.data.title ?? 'No title');
+
+  return new Response(body, {
+    headers: {
+      'content-type': 'image/png',
+    },
+  });
 }
