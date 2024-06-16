c931c7dc75829bba8888cdf12ba349a80180f82e
satoru <contac@ear-diary.com>
2024-06-16 11:59:09 +0000

    記事一覧作った

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,46 +8,33 @@ import playformInline from '@playform/inline';
 import icon from 'astro-icon';
 import robotsTxt from 'astro-robots-txt';
 import { defineConfig } from 'astro/config';
-
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind(),
-        robotsTxt({
-            sitemap: 'https://ear-diary.comsitemap-index.xml',
-        }),
-        sitemap(),
-        partytown({
-            config: {
-                forward: ['dataLayer.push'],
-            },
-        }),
-        icon(),
-        react(),
-        itsmatteomanfsecurityTxt({
-            contact: 'mailto:contact@ear-diary.com',
-            preferredLanguages: ['en', 'ja'],
-        }),
-        playformInline(),
-        playformCompress(),
-    ],
-    site: 'https://ear-diary.com',
-    image: {
-        remotePatters: [
-            {
-                hostname: '**.microcms-assets.io',
-            },
-        ],
-    },
-    prefetch: true,
-    prefetch: {
-        prefetchAll: true,
-        defaultStrategy: 'viewport',
-    },
-    markdown: {
-        remarkRehype: {
-            footnoteLabel: '脚注',
-            footnoteBackLabel: 'コンテンツに戻る',
-        },
-    },
+  integrations: [tailwind(), robotsTxt({
+    sitemap: 'https://ear-diary.comsitemap-index.xml'
+  }), sitemap(), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  }), icon(), react(), itsmatteomanfsecurityTxt({
+    contact: 'mailto:contact@ear-diary.com',
+    preferredLanguages: ['en', 'ja']
+  }), playformInline(), playformCompress()],
+  site: 'https://ear-diary.com',
+  image: {
+    remotePatters: [{
+      hostname: '**.microcms-assets.io'
+    }]
+  },
+  prefetch: true,
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'viewport'
+  },
+  markdown: {
+    remarkRehype: {
+      footnoteLabel: '脚注',
+      footnoteBackLabel: 'コンテンツに戻る'
+    }
+  }
 });
