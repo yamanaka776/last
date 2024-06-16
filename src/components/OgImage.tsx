c099127695cbaa3b44201c787f5b2854e9afd094
satoru <contac@ear-diary.com>
2024-06-16 13:51:56 +0000

    lint

--- src/components/OgImage.tsx
+++ src/components/OgImage.tsx
@@ -1,67 +1,69 @@
-import satori from 'satori';
-import sharp from 'sharp';
+import satori from "satori";
+import sharp from "sharp";
 
 export async function getOgImage(text: string) {
-    const fontData = (await getFontData()) as ArrayBuffer;
-    const svg = await satori(
-        <div
-            style={{
-                height: '100%',
-                width: '100%',
-                display: 'flex',
-                justifyContent: 'space-between',
-                flexDirection: 'column',
-                backgroundColor: 'rgb(55,65,81)',
-                fontWeight: 600,
-                padding: 48,
-                border: '48px solid rgb(31,41,55)',
-            }}
-        >
-            <div style={{ color: '#fff', fontSize: 64, maxWidth: 1000 }}>{text}</div>
-            <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-                <div
-                    style={{
-                        color: '#d1d5db',
-                        fontSize: 48,
-                        display: 'flex',
-                        alignItems: 'center',
-                    }}
-                >
-                    耳日記
-                </div>
-            </div>
-        </div>,
-        {
-            width: 1200,
-            height: 630,
-            fonts: [
-                {
-                    name: 'Noto Sans JP',
-                    data: fontData,
-                    style: 'normal',
-                },
-            ],
-        },
-    );
+	const fontData = (await getFontData()) as ArrayBuffer;
+	const svg = await satori(
+		<div
+			style={{
+				height: "100%",
+				width: "100%",
+				display: "flex",
+				justifyContent: "space-between",
+				flexDirection: "column",
+				backgroundColor: "rgb(55,65,81)",
+				fontWeight: 600,
+				padding: 48,
+				border: "48px solid rgb(31,41,55)",
+			}}
+		>
+			<div style={{ color: "#fff", fontSize: 64, maxWidth: 1000 }}>{text}</div>
+			<div style={{ display: "flex", justifyContent: "space-between" }}>
+				<div
+					style={{
+						color: "#d1d5db",
+						fontSize: 48,
+						display: "flex",
+						alignItems: "center",
+					}}
+				>
+					耳日記
+				</div>
+			</div>
+		</div>,
+		{
+			width: 1200,
+			height: 630,
+			fonts: [
+				{
+					name: "Noto Sans JP",
+					data: fontData,
+					style: "normal",
+				},
+			],
+		},
+	);
 
-    return await sharp(Buffer.from(svg)).png().toBuffer();
+	return await sharp(Buffer.from(svg)).png().toBuffer();
 }
 
 async function getFontData() {
-    const API = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700`;
+	const API = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@700`;
 
-    const css = await (
-        await fetch(API, {
-            headers: {
-                'User-Agent':
-                    'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1',
-            },
-        })
-    ).text();
+	const css = await (
+		await fetch(API, {
+			headers: {
+				"User-Agent":
+					"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+			},
+		})
+	).text();
 
-    const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/);
+	const resource = css.match(
+		/src: url\((.+)\) format\('(opentype|truetype)'\)/,
+	);
 
-    if (!resource) return;
+	if (!resource) return;
 
-    return await fetch(resource[1]).then((res) => res.arrayBuffer());
+	return await fetch(resource[1]).then((res) => res.arrayBuffer());
 }
--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,22 +1,22 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
+import { getCollection } from "astro:content";
+import rss from "@astrojs/rss";
 export async function GET(context) {
-    const blogs = await getCollection('blog');
-    return rss({
-        // 出力されるXMLの`<title>`フィールド
-        title: 'ear-diary',
-        // 出力されるXMLの`<description>`フィールド
-        description: 'ear-diary',
-        // エンドポイントのコンテキストからプロジェクトの"site"を取得
-        // https://docs.astro.build/ja/reference/api-reference/#contextsite
-        site: context.site,
-        // 出力されるXMLの<item>の
-        // コンテンツコレクションやglobインポートを利用した例については「`items`の生成」セクションをご覧ください
-        items: blogs.map((conten) => ({
-            title: conten.data.title,
-            pubDate: conten.data.pubDate,
-            link: `blog/${conten.slug}/`,
-        })),
-        stylesheet: '/rss/styles.xsl',
-    });
+	const blogs = await getCollection("blog");
+	return rss({
+		// 出力されるXMLの`<title>`フィールド
+		title: "ear-diary",
+		// 出力されるXMLの`<description>`フィールド
+		description: "ear-diary",
+		// エンドポイントのコンテキストからプロジェクトの"site"を取得
+		// https://docs.astro.build/ja/reference/api-reference/#contextsite
+		site: context.site,
+		// 出力されるXMLの<item>の
+		// コンテンツコレクションやglobインポートを利用した例については「`items`の生成」セクションをご覧ください
+		items: blogs.map((conten) => ({
+			title: conten.data.title,
+			pubDate: conten.data.pubDate,
+			link: `blog/${conten.slug}/`,
+		})),
+		stylesheet: "/rss/styles.xsl",
+	});
 }
